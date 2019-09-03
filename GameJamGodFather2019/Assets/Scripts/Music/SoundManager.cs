5cf5226cdf029870f57ad1b275f73b6f0996660a
Tristan Record <45321591+RecordTristan@users.noreply.github.com>
2019-09-03 11:38:31 +0200

    Add sfx sounds player

---
+++ GameJamGodFather2019/Assets/Scripts/Music/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private AudioSource music;
+    private AudioSource sfx;
+
+    public AudioClip musicPrincipal;
+    [Header("SFXs")]
+    public AudioClip[] insults;
+    public AudioClip gresillement;
+    public AudioClip swordTouch;
+    public AudioClip swordClash;
+    public AudioClip swordBlock;
+
+
+    void Awake(){
+        music = GetComponents<AudioSource>()[0];
+        sfx = GetComponents<AudioSource>()[1];
+    }
+
+    #region SFXs
+    public void Insult(int id){
+        sfx.PlayOneShot(insults[id]);
+    }
+    public void Gresille(){
+        sfx.PlayOneShot(gresillement);
+    }
+    public void SwordTouch(){
+        sfx.PlayOneShot(swordTouch);
+    }
+    public void SwordClash(){
+        sfx.PlayOneShot(swordClash);
+    }
+    public void SwordBlock(){
+        sfx.PlayOneShot(swordBlock);
+    }
+
+    #endregion
+}
